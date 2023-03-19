e2c34318856ec155d8ed90c22916726da455a3fa
Ensar <ansartohti83@gmail.com>
2023-03-19 20:05:05 +0300

    Initial commit

---
+++ src/test/java/com/cydeo/step_definitions/Fidexio_Login_StepDefinitions.java
@@ -0,0 +1,2 @@
+package com.cydeo.step_definitions;public class Fidexio_Login_StepDefinitions {
+}
---
+++ src/test/java/com/cydeo/step_definitions/Fidexio_Logout_StepDefinitions.java
@@ -0,0 +1,2 @@
+package com.cydeo.step_definitions;public class Fidexio_Logout_StepDefinitions {
+}
